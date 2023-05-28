7fd73c1acd79f360ea11f026177798c62ef5811f
DE-TOX <72025600+DE-TOX@users.noreply.github.com>
2023-05-28 11:40:38 +0530

    Time: 3 ms (49.72%), Space: 6 MB (53.66%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        for (int i =0; i<31;i++){
+        for (int i =0; i<=30;i++){
             int a = pow(2,i);
             if(n==a){
                 return true;
